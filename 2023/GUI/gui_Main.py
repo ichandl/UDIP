09d6f5a6d3ada9a170eb9557f6e6919914a55167
Isaac <ichandl@udel.edu>
2022-09-13 23:44:55 -0400

    gui v0.3

--- 2023/GUI/gui_Main.py
+++ 2023/GUI/gui_Main.py
@@ -1,5 +1,8 @@
 '''
-Main GUI Window all GUI setup and computation is done here
+Main GUI Window 
+All GUI setup and computation is done here
+
+If you are planning on reading this code, you better buckle up
 '''
 # Import
 # Custom Imports
@@ -41,8 +44,14 @@ class MasterWindow(Ui_UDIP_Viewer):
         self.ind_burst = []
         self.ind_constant = []
 
-        
+        # List of dictionaries used to do some things
+        self.data_sensor = []
+        self.data_constant = []
+        self.sweep_time = []
+ 
         # Load the UDIP Data
+        print("Loading Data")
+
         self.Load_data()
 
     def setupui(self, MainWindow: QtWidgets.QMainWindow):
@@ -56,136 +65,123 @@ class MasterWindow(Ui_UDIP_Viewer):
         self.PlotButton.clicked.connect(lambda: self.SendPlot())        # Plot Button Pushes Selection To Plot
         self.NextButton.clicked.connect(lambda: self.IndexSelectionCombo.setCurrentIndex(self.IndexSelectionCombo.currentIndex() + 1))
         self.PreviousButton.clicked.connect(lambda: self.IndexSelectionCombo.setCurrentIndex(self.IndexSelectionCombo.currentIndex() - 1))
+        self.TimeMono.editingFinished.connect(lambda: self.SendPlot())
+        self.StartTime.editingFinished.connect(lambda: self.SendPlot())
+        self.EndTime.editingFinished.connect(lambda: self.SendPlot())
 
         # If a GUI Button is Updated, refresh the display
         self.TimeIndexButton.toggled.connect(lambda: self.updGui())
         self.IndexIndexButton.toggled.connect(lambda: self.updGui())
-        self.PlotTypeCombo.activated.connect(lambda: self.updGui())
         
         # Update the GUI Logic ()
+        self.IndexSelectionCombo.addItems([str(x) for x in self.ind_medium])
+        self.IndexSelectionCombo.currentIndexChanged.connect(lambda: self.SendPlot())
         self.updGui()
 
-        
-
+    # We do some logic updating
     def updGui(self):
         """
         Used to Update the GUI's logic and display
 
-        Partly aesthetic, critical controls start hidden and are revealed depending on the plot type
-        If almost any GUI Button is CLicked or GUI Element Updated, it should point here to update the logic
+        This whole function is retarded ... for now
         """
         # This whole section edits the gui to display the currently selected indexing method (direct index or time-based)
-        self.TimeSelectionBox.setEnabled(self.TimeIndexButton.isChecked())                 
+        self.SweepTimeBox.setEnabled(self.TimeIndexButton.isChecked())                 
         self.IndexSelectionBox.setEnabled(self.IndexIndexButton.isChecked())
-
-        # Modify the gui to display sweep or sensor/constant stuff
-        if self.PlotTypeCombo.currentText() == ("Constant" or "Sensor"):
-            # Set the time controls to A->B and not point
-            self.TimeMonoBox.setVisible(False)
-            self.TimeStartStopBox.setVisible(True)
-
-            # Sets Amp Control to not visible (Or Visible)
-            self.SweepSettingsBox.setVisible(False)
-        else:
-            # Same as above but inverse
-            self.TimeMonoBox.setVisible(True)
-            self.TimeStartStopBox.setVisible(False)
-            
-            self.SweepSettingsBox.setVisible(True)
-
-        
-        # Populate GUI With Relevant Data Information
-        self.IndexSelectionCombo.clear()
-        if self.PlotTypeCombo.currentText() == "Medium Sweep":
-            self.IndexSelectionCombo.addItems([str(x) for x in self.ind_medium])
-        elif self.PlotTypeCombo.currentText() == "Burst Sweep":
-            self.IndexSelectionCombo.addItems([str(x) for x in self.ind_burst])
-        elif self.PlotTypeCombo.currentText() == "Large Sweep":
-            self.IndexSelectionCombo.addItems([str(x) for x in self.ind_large])
-        elif self.PlotTypeCombo.currentText() == "Sensor":
-            self.IndexSelectionCombo.addItems([str(x) for x in self.ind_sensor])
-        elif self.PlotTypeCombo.currentText() == "Constant":
-            self.IndexSelectionCombo.addItems([str(x) for x in self.ind_sensor])
 
     # Main Plotting Function
     def SendPlot(self):
         """
-        Master plotting function, manages and calls all other plot functions
+        Master plotting function, prepares and calls the appropriate plot functions
         """
-        # print("SendPlot Received")
+        try:
+            # He will never be debugging
+            # print("SendPlot Received")
 
-        # Clear Current Plot
-        self.PlotDisplay.plotItem.clear()
+            # Clear Current Plot
+            self.PlotDisplay.plotItem.clear()
 
-        # Does one of three things: 
-        # 1) Creates a dict of the "start" or "stop" times for a constant/sensor plot                 
-        # 2) Finds the closest sweep index for the provided time                      
-        # 3) Grabs the user-selected index from the IndexSelectionCombo ComboBox
-        # 
-        # This information is then used to inform the relevent plotting function on what to display
-        if self.TimeIndexButton.isChecked() and (self.PlotTypeCombo.currentText() == ("Constant" or "Sensor")):
-            indexNum = {"start": self.find_closest(self.StartTime.text()), "stop": self.find_closest(self.EndTime.text())}
-        elif self.TimeIndexButton.isChecked():
-            indexNum = self.find_closest(self.TimeMono.text())
-        else:
-            indexNum = int(self.IndexSelectionCombo.currentText())
-
-        # Plot the appropriate sweep
-        if self.PlotTypeCombo.currentText() == ("Constant" or "Sensor"):
-            self.Plot_const(indexNum)
-        else:
-            self.Plot_sweep(indexNum)
-            
+            # Plot the appropriate sweep
+            if self.ControlTabs.currentIndex() == 1:
+                indexNums = [self.find_closest("Sweep",self.StartTime.value()),self.find_closest("Sweep",self.EndTime.value())]
+                # Plot Constant
+                self.Plot_const()
+            else:
+                # Plot Sweep
+                # Find The Corrext Index
+                if self.TimeIndexButton.isChecked():
+                    indexNum = self.find_closest("Sweep",self.TimeMono.value())
+                else:
+                    indexNum = int(self.IndexSelectionCombo.currentText())
+                
+                self.Plot_sweep(indexNum)
+        except:
+            pass
 
     # Loads and indexes the data
     def Load_data(self):
+        # This is the only thing I use the UDIP_Lib for, don't want to move all of that stuff over
         self.packets = np.array(UDIP_Lib.readFile(self.filename))
 
-        # Indexes all packets
+        # Catalogs all packets i is index, p is the packet
         for i, p in enumerate(self.packets):
             if(p.pcktType == 0x01):           # Sensor Packet Type
+                # Add the sensor info into a list of dictionary
+                self.data_sensor.append({"time": p.tInitial,"index": i,"accY": p.accX,"accY": p.accY,"accZ": p.accZ,"accH": p.accH,"gyroX": p.gyroX, "gyroY": p.gyroY, "gyroZ": p.gyroZ,"magX": p.magX,"magY": p.magY,"magZ": p.magZ,"tmpD": p.temperature_d,"tmpP": p.temperature_p,"tmpS": p.temperature_s})
+                # This isn't used anywhere ... yet
                 self.ind_sensor.append(i)
             elif(p.pcktType == 0x10):         # Medium Sweep Packet Type
+                # Add to time dictionary
+                self.sweep_time.append({"time": p.tInitial, "index": i})
+                # Add to list of index
                 self.ind_medium.append(i)
+            elif(p.pcktType == 0x30):         # Constant Sweep Packet Type
+                self.ind_constant.append(i)
+            # These Two Arent Used
             elif(p.pcktType ==  0x11):        # Large Sweep Packet Type
                 self.ind_large.append(i)
             elif(p.pcktType == 0x20):         # Burst Sweep Packet Type
                 self.ind_burst.append(i)
-            elif(p.pcktType == 0x30):         # Constant Sweep Packet Type
-                self.ind_constant.append(i)
-  
-    # ---Functions used to parse the packets and display the data---
-
-    # finds the packet that is closest to the selected time
-    def find_closest(self, time):
-        #returns index in array that is closest to the user provided time
-        
-
-
-
-        n = len(array)
-        if (time < self.packets[array[0]].tInitial):
-            return 0
-        elif (time > self.packets[array[-1]].tInitial):
-            return n-1
-        else:
-            b = self.packets[array[0]].tInitial
-            a = self.packets[array[0]].tInitial
-            i = 1
-            while (a < time):
-                b = a
-                a = self.packets[array[i + 1]].tInitial
-                i = i + 1
-            if (time - b < a - time):
-                return i - 1
             else:
-                return i
+                print("How?")
+            
+    # finds the packet that is closest to the selected time(s)
+    def find_closest(self, type: str, time: float):
+        #returns index in array that is closest to the user provided time
+        # Really jank could be improved
+        try:
+            if type == "Sweep":
+                # Goes through the list of times
+                for i in self.sweep_time:
+                    # once the time of the current selection is bigger than the requested time, return the previous one
+                    if i["time"] > time*1000:
+                        return i["index"]
+            # Does the same thing as sweep but from the sensor one
+            elif type == "Sensor":
+                # Goes through the list of times
+                for i in self.data_sensor:
+                    # once the time of the current selection is bigger than the requested time, return the previous one
+                    if i["time"] > time*1000:
+                        return i["index"]
+            # Does the same thing as sweep but from the constant one; not implimented yet
+            # if type == "Constant":
+            #     # Goes through the list of times
+            #     for i in self.data_sensor:
+            #         # once the time of the current selection is bigger than the requested time, return the previous one
+            #         if i["time"] > time:
+            #             return self.sweep_time[i]["index"]
+
+            # if the provided time is larger than any possible time, return the final index
+            return i["index"]
+        except:
+            pass
 
     def Plot_sweep(self, indexNum):    
         # Using the chosen packet number, we plot the corresponding sweep
         Selection = self.packets[indexNum]
 
         # Define the plotted variables
+        print(Selection.sweep.sweepVoltage)
         x = Selection.sweep.sweepVoltage
 
         # The current was amplified three times, user can choose amp level to display (or multiple)
@@ -199,12 +195,12 @@ class MasterWindow(Ui_UDIP_Viewer):
             y2 = Selection.sweep.adc2Curr
             self.PlotDisplay.plot(x,y2,pen="r", name="Amp 2")
 
-    def Plot_const(self, indexNum):
+    def Plot_const(self, indexNums):
         # Using the chosen packet number, we plot the corresponding sweep
-        SelectionStart = self.packets[indexNum[0]]
-        SelectionEnd = self.packets[indexNum[1]]
+        SelectionStart = self.packets[indexNums[0]]
+        SelectionEnd = self.packets[indexNums[1]]
 
-        # Define the plotted variables
+        # This plot is always over time, so x is the range between the start and stop
         x = Selection.sweep.sweepVoltage
 
         # The current was amplified three times, user can choose amp level to display (or multiple)
@@ -226,17 +222,18 @@ class MasterWindow(Ui_UDIP_Viewer):
 
 
 
-# Main        
+# -- Main --       
 import sys
-
-# Create GUI application
+# GUI Setup Process via PyQt5
+# 0) Start a PyQt app, required to do anything else
 app = QtWidgets.QApplication(sys.argv)
-
-# Order:
-# 1) Get the default QT main window   2) Initialize the gui (Main_Window) defined in this file   3) run the setupui function   4) actually display the window
+# 1) Get the default QT main window
 MainWindow = QtWidgets.QMainWindow()
+# 2) Initialize the gui (The class that is defined in this file) (Also runs the init function)
 gui = MasterWindow()
+# 3) Run the setupui function, described there
 gui.setupui(MainWindow)
+# 4) Actually display the window
 MainWindow.show()
-
+# 5) No Idea
 app.exec_()
